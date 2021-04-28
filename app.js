40058a1a7f6930fc6a7383eb51cb69bdc2ea1022
Corentin Clerc <ccorentin92@gmail.com>
2021-04-28 09:51:42 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const sauceRoutes = require('./routes/sauce');
+
+
+mongoose.connect('mongodb+srv://Co21c:iv2rd4500@cluster0.t5vs2.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+const app = express();
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/sauces', sauceRoutes);
+
+
+
+
+
+module.exports = app;
